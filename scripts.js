6c3fc5d121964c25b7ae4960a6623c82d5daac60
vitormedeiros911 <vitormedeiros911@outlook.com>
2021-04-03 12:04:03 -0300

    Desafio Foodfy - primeiro commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+  card.addEventListener("click", function () {
+    modalOverlay.classList.add("active");
+    const img = card.querySelector("img").src;
+    modalOverlay.querySelector("h3").innerHTML = card.querySelector("h3").textContent;
+    modalOverlay.querySelector("p").innerHTML = card.querySelector("p").textContent;
+    modalOverlay.querySelector("img").src = img;
+  })
+}
+
+document.querySelector('.close').addEventListener("click", function () {
+  modalOverlay.classList.remove("active");
+})
